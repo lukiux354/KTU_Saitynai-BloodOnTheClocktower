cc42a66b9f80f4f906def0364025db7885f4d2fb
Lukas <luksonas@gmail.com>
2024-12-12 01:14:04 +0200

    front attempt

---
+++ Saitynai/DemoFront/forum/src/App.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const App = () => {
+    const [scripts, setScripts] = useState([]);
+
+    useEffect(() => {
+        const loadTopics = async () => {
+            //const response = await axios.get('https://sea-lion-app-vayj4.ondigitalocean.app/api/topics');
+            //const response = await axios.get('http://localhost:5106/api/topics');
+			const response = await axios.get('https://urchin-app-6crcv.ondigitalocean.app/api/scripts');
+            console.log(response.data.resource);
+            setTopics(response.data.resource);
+        };
+
+        loadTopics();
+    }, []);
+
+    return (
+        <>
+        {scripts.map((script, i) => (
+            <p key={i}>{script.resource.title} {script.resource.description}</p>
+        ))}
+        </>
+    )
+}
+
+export default App;
